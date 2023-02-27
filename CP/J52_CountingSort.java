b75832a8d1c0bc62c664cfa5deae5533ba02bb16
Rishabh Deo Singh <99266473+Rishabh-mikku@users.noreply.github.com>
2023-02-28 01:28:26 +0530

    P-19: Counting Sort

--- CP/J52_CountingSort.java
+++ CP/J52_CountingSort.java
@@ -1,3 +1,21 @@
+// Counting Sort Algorithm: Overview, Time Complexity & More
+// Lesson 29 of 68By Simplilearn
+
+// Last updated on Feb 17, 202318543
+// Everything You Need to Know About the Counting Sort Algorithm
+// PreviousNext
+// Table of Contents
+// What Is a Counting Sort Algorithm?Working on Counting Sort AlgorithmPseudo Code and Algorithm of Counting Sort AlgorithmThe Complexity of the Counting Sort AlgorithmComparison With Other Sorting AlgorithmsView More
+// Harold Seward discovered Counting Sort in 1954. Counting sort is a linear sorting algorithm with asymptotic complexity O(n+k). The Counting Sort method is a fast and reliable sorting algorithm. Counting sort, unlike bubble and merge sort, is not a comparison-based algorithm. It avoids comparisons and takes advantage of the array's O(1) time insertions and deletions. 
+
+// The Counting Sort algorithm sorts keys that are small integers and fall inside a particular range. It works by calculating the number of elements with each unique key value. After that, it performs specific mathematical calculations to sort each key value.
+
+// What Is a Counting Sort Algorithm?
+// Counting sort is an integer sorting algorithm used in computer science to collect objects according to keys that are small positive integers.
+// It works by determining the positions of each key value in the output sequence by counting the number of objects with distinct key values and applying prefix sum to those counts.
+// Because its running duration is proportional to the number of items and the difference between the maximum and minimum key values, it is only suited for direct usage when the number of items is not much more than the variation in keys.
+// It's frequently used as a subroutine in radix sort, a more efficient sorting method for larger keys.
+
 public class J52_CountingSort{
 
     public static int[] CountSort(int[] arr, int n){
